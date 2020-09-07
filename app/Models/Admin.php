9a4b50ca7c0f01d2ce8969b32e9aa74f3dfe999f
Mohamed <mohammed.aitmiloud@gmail.com>
2020-09-07 15:00:49 +0100

    backend admin authentication

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-        use Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'admin.php';
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/admin', 'admin.dashboard.index');
+Auth::routes();
